b890d7d936c9c7697932403f04c3962fa8be95b7
dieselniu <niu853273494@qq.com>
2021-05-04 09:06:02 +0800

    完成登录模块的鉴权功能
    即用户输入用户名和密码，server返回这个用户是否是合法用户。

--- src/main/java/com/github/DieselNiu/configuration/JavaConfiguration.java
+++ src/main/java/com/github/DieselNiu/configuration/JavaConfiguration.java
@@ -1,9 +1,5 @@
 package com.github.DieselNiu.configuration;
 
-import com.github.DieselNiu.Service.OrderService;
-import com.github.DieselNiu.Service.UserService;
-import com.github.DieselNiu.mapper.UserMapper;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
--- src/main/java/com/github/DieselNiu/mapper/UserMapper.java
+++ src/main/java/com/github/DieselNiu/mapper/UserMapper.java
@@ -1,6 +1,6 @@
 package com.github.DieselNiu.mapper;
 
-import com.github.DieselNiu.Service.User;
+import com.github.DieselNiu.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
